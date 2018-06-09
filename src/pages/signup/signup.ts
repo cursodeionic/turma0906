6939ede0cd473560eadd016784aa4bed3621b876
Jeff Souza <hsinfo@gmail.com>
2018-06-09 18:19:39 -0300

    Save to api

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,3 +1,4 @@
+import { UserServiceProvider } from './../../providers/user-service/user-service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
@@ -9,7 +10,10 @@ import { NavController, NavParams } from 'ionic-angular';
 export class SignupPage {
   public form: FormGroup;
 
-  constructor(public navCtrl: NavController, private builder: FormBuilder) {
+  constructor(public navCtrl: NavController, 
+    private builder: FormBuilder,
+    private _service: UserServiceProvider
+  ) {
     this.form = this.builder.group({
       'email': ['', Validators.compose([Validators.required])],
       'name': ['', Validators.compose([Validators.required])]
@@ -20,6 +24,11 @@ export class SignupPage {
   }
 
   submit(){
+    this._service.createUser(this.form.controls.values).subscribe(
+      data => {
+        console.log(data);
+      }
+    );
     console.log(this.form.controls.values);
   }
 
